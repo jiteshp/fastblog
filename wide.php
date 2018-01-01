126fe8d5936fa4afba2b0532cc01a5dcc3d13c3e
Jitesh Patil <jitesh@yapapaya.com>
2018-01-01 09:59:08 +0530

    Fixes #7

---
+++ wide.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template name: Wide Template
+ *
+ * Displays a full width page.
+ *
+ * @package FastBlog
+ * @author Jitesh Patil <jitesh.patil@gmail.com>
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+<!-- site-content -->
+<div class="site-content">
+	<main class="main" role="main">
+		<?php
+		while ( have_posts() ) {
+			the_post();
+			get_template_part( 'template-parts/content', 'page' );
+		}
+		?>
+	</main>
+</div><!-- /site-content -->
+
+<?php
+get_footer();
