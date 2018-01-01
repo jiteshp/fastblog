e7730589e9b0c087649f68eefb42fd02bd22c6bf
Jitesh Patil <jitesh@yapapaya.com>
2018-01-01 10:21:44 +0530

    Fixes #7

--- footer.php
+++ footer.php
@@ -14,13 +14,16 @@ do_action( 'fastblog_after_content' );
 ?><footer class="site-footer" role="contentinfo">
 	<div class="container">
 		<span class="copyright">
-			<?php printf( esc_html__( 'Copyright &copy; %1$s ', 'fastblog' ), date( esc_html__( 'Y', 'fastblog' ) ) ); ?>
+			<?php printf( esc_html__( 'Copyright &copy; %1$s', 'fastblog' ), date( esc_html__( 'Y', 'fastblog' ) ) ); ?>
 			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 		</span>
 
 		<span class="credits">
-			<?php esc_html_e( 'FastBlog WordPress theme by', 'fastblog' ); ?>
-			<a href="https://www.jiteshpatil.com/" rel="designer"><?php esc_html_e( 'Jitesh Patil', 'fastblog' ); ?></a>
+			<?php esc_html_e( 'Powered by', 'fastblog' ); ?>
+			<a href="https://wordpress.org/" rel="generator"><?php esc_html_e( 'WordPress', 'fastblog' ); ?></a>
+			<?php esc_html_e( '& the', 'fastblog' ); ?>
+			<a href="https://wordpress.org/themes/fastblog/" rel="designer"><?php esc_html_e( 'FastBlog', 'fastblog' ); ?></a>
+			<?php esc_html_e( 'theme', 'fastblog' ); ?>
 		</span>
 	</div>
 </footer><!-- /site-footer -->
