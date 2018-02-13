a539bdf0c6cd9d7874c870c88c22226792596bf2
Jitesh Patil <jitesh@yapapaya.com>
2018-02-13 21:08:49 +0530

    Changes bold font weight

--- functions.php
+++ functions.php
@@ -74,7 +74,7 @@ if ( ! function_exists( 'fastblog_setup' ) ) {
 
 		add_editor_style( array(
 			get_template_directory_uri() . '/style-editor.css',
-			'https://fonts.googleapis.com/css?family=Lato:400,400i,900,900i|Inconsolata',
+			'https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i|Inconsolata',
 		) );
 	}
 } // End if().
@@ -110,7 +110,7 @@ add_action( 'widgets_init', 'fastblog_sidebar' );
  */
 function fastblog_assets() {
 	wp_enqueue_style( 'fastblog-style', get_stylesheet_uri() );
-	wp_enqueue_style( 'fastblog-fonts', 'https://fonts.googleapis.com/css?family=Lato:400,400i,900,900i|Inconsolata' );
+	wp_enqueue_style( 'fastblog-fonts', 'https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i|Inconsolata' );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
