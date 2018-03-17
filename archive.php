6e0646cba3eb4b9bb2719fb8d3293d80f9f10bd6
Jitesh Patil <jitesh@yapapaya.com>
2018-03-17 07:43:19 +0530

    Adds support for archive description

--- archive.php
+++ archive.php
@@ -17,7 +17,10 @@ get_header(); ?>
 		<div class="row">
 			<main class="main col-xs-12 col-md-8" role="main">
 				<header class="page-header">
-					<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+					<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="page-description">', '</div>' );
+					?>
 				</header>
 
 				<?php
